dd79278a646150f165336a4ff7047b2a4c9c7f9d
Jakub Bogdański <jakubbogd09@gmail.com>
2021-06-08 10:00:03 +0200

    version before fun

--- src/AppState.hs
+++ src/AppState.hs
@@ -1,125 +1,89 @@
-module CandW where
+module Main where
 
-import Data.Map as Map
-import System.Random
+import CandW
+import Graphics.Gloss
+import Graphics.Gloss.Interface.IO.Game
 
-data Direction = UP | DOWN |LEFT | RIGHT deriving (Eq, Ord)
-type Wall = [(Int, Int)]
-type Car = [(Int, Int)]
-type Walls=[Wall]
+window :: Display
+window = InWindow "Haskell Car Game" (1000, 600) (100, 100)
 
+background :: Color
+background = black
 
-cols::Int
-rows::Int
-cols = 50
-rows = 33
+render :: GameState -> IO Picture
+render gameState =return (pictures $   [ fillRectangle black (16, 0) (0,0)
+                                ] ++
+                                  fmap (convertToPicture white) car ++ 
+                                  createwalls walls ++
+                                  pointsPicture++
+                                  lifesPicture++
+                                  recordPicture++
+                                  gameOverPicture)
+    where   car = getCar gameState 
+            walls = getWalls gameState
+            point = getPoints gameState
+            life = getLifes gameState
+            record=getRecord gameState
+            convertToPicture :: Color -> (Int, Int) -> Picture
+            convertToPicture color' (x, y) = fillRectangle color' (toFloat (x, y)) (20, 20)
+            fillRectangle color' (tx, ty) (w, h) =  color color' $ 
+                                                    scale 1 (-1) $ 
+                                                    translate (tx * 20 - 500) (ty * 20 - 300) $ 
+                                                    rectangleSolid w h
+            createwalls::Walls->[Picture]
+            createwalls (w:ws)=(fmap (convertToPicture blue) w)++(createwalls ws)
+            createwalls []=[]
+            toFloat (x, y) = (fromIntegral x, fromIntegral y)
+            gameOverPicture =   if isGameOver gameState 
+                                then [  color red $ 
+                                        translate (-200) 0 $ 
+                                        scale 0.5 0.5 $ 
+                                        text "GAME OVER"
+                                     ,  color blue $ 
+                                        translate (-175) (-50) $ 
+                                        scale 0.2 0.2 $ 
+                                        text "Press ENTER to try again." ] 
+                                else []
+            pointsPicture=     [color yellow $ 
+                                translate 450 250 $ 
+                                scale 0.25 0.25 $ 
+                                text (show point)]
+            recordPicture=     [color yellow $ 
+                                translate 450 200 $ 
+                                scale 0.25 0.25 $ 
+                                text (show record)]
+            lifesPicture =     [color red $ 
+                                translate (-450) 250 $ 
+                                scale 0.25 0.25 $ 
+                                text (show life)]
+                                                        
+update :: Float -> GameState -> IO GameState
+update _ (GameState car walls points direction gameOver stdgen stdgen2 lifes record) =  do
+                        writeFile "record.txt" (show record)
+                        if gameOver
+                            then return (GameState car (genWalls 3 stdgen2) points direction gameOver stdgen stdgen2 lifes record)
+                            else return (GameState newCar newWalls newPoints direction newGameOver newStdGen newStdGen2 newlifes newRecord)
+    where   
+            GameState newCar _ _ _ _ _ _ newlifes _= move (GameState car walls points direction gameOver stdgen stdgen2 lifes record)
+            (newWalls,newStdGen,newStdGen2,wasWallsavoided) = moveWalls (GameState car walls points direction gameOver stdgen stdgen2 lifes record)
+            newGameOver = checkGameOver (GameState car walls points direction gameOver stdgen stdgen2 lifes record)
+            newPoints
+                |wasWallsavoided=points+1
+                |otherwise=points
+            newRecord
+                |record<newPoints=newPoints
+                |otherwise=record
+                
+handleKeys::Event->GameState->IO GameState
+handleKeys (EventKey (SpecialKey KeyLeft ) Down _ _) gameState = return (changeDirection gameState LEFT)
+handleKeys (EventKey (SpecialKey KeyRight) Down _ _) gameState = return (changeDirection gameState RIGHT)  
+handleKeys (EventKey (SpecialKey KeyUp   ) Down _ _) gameState = return (changeDirection gameState UP)
+handleKeys (EventKey (SpecialKey KeyDown ) Down _ _) gameState = return (changeDirection gameState DOWN) 
+handleKeys (EventKey (SpecialKey KeyEnter) Down _ _) gameState =    if isGameOver gameState then return (initialGameState False record) else return gameState
+    where record=getRecord gameState
+handleKeys _ gameState = return gameState
 
-directionVectorMap :: Map Direction (Int, Int)
-directionVectorMap = Map.fromList $ zip [UP,DOWN, LEFT, RIGHT] 
-                                        [(0, -1), (0, 1),(-1, 0), (1, 0)]
-
-wasWallsHit::GameState->Bool
-wasWallsHit (GameState car (w:ws) a b c d e f)= wasWallHit car w || wasWallsHit (GameState car ws a b c d e f)
-wasWallsHit (GameState _ [] _ _ _ _ _ _)=False
-
-wasWallHit::Car->Wall->Bool
-wasWallHit car wall=wasWallhit
-        where
-                wasWallhit = (headX<=wallX2 && headX>=wallX1 &&wallY==headY) ||
-                        (headX<=wallX2 && headX>=wallX1 &&wallY==(headY-1))||
-                        wallY==(headY-2) && ((wallX1<=headX-1 && headX-1<=wallX2) || (wallX1<=headX && headX<=wallX2) || (wallX1<=headX+1 && headX+1<=wallX2))
-                (headX, headY) = head car
-                (wallX1,wallY) = head wall
-                (wallX2,_) = last wall
-
-move':: (Int,Int)->Car->Car
-move' (_,_) []=[]
-move' (a,b) ((c,d):xs) =(a+c,b+d): move' (a,b) xs
-
-move :: GameState->GameState
-move (GameState car wall a dir aa aaa lifes aaaa)= if wasWallsHit (GameState car wall a dir aa aaa lifes aaaa)
-                            then (GameState car wall a dir aa aaa (lifes-1) aaaa)
-                            else (GameState newcar wall a dir aa aaa lifes aaaa)
-    where   newcar = move' (directionVectorMap ! dir) car
-
-gennewWall::Int->Int->Int->Int->Wall
-gennewWall x y rn w=[((min (max (x+rn+i) 0) (min (x+rn+i) cols)),(y-1))|i<-[0..w]]
-
-gennewWalls::[Int]->Int->[Int]->[Int]->Walls
-gennewWalls (x:xs) y (r:rs) (w:ws)=gennewWall x y r w:gennewWalls xs y rs ws
-gennewWalls [] _ _ _ =[]
-gennewWalls _ _ [] _ =[]
-gennewWalls _ _ _ [] =[]
-
-getHeads::Walls->[Int]
-getHeads (w:ws)=(getHead w):(getHeads ws)
-getHeads []=[]
-
-getHead::Wall->Int
-getHead ((a,_):_) =a
-getHead [] =0
-
-moveWalls:: GameState-> (Walls,StdGen,Bool)
-moveWalls (GameState _ walls _ _ _ ran _ _)= (gennewWalls wallXs wallY' rns widths,ran',newPoint)
-        where
-                (_,wallY)=head (head walls)
-                wallXs=getHeads walls
-                (wallY',rm)
-                        |wallY<2 = (rows-2,10)
-                        |otherwise =(wallY,1)
-                newPoint
-                        |rm==1=False 
-                        |otherwise =True
-                (_,ran')=randomR (-1*rm, 1*rm) ran
-                rns=getRandoms ran walls rm
-                widths=getWidths ran walls wallY
-
-getRandoms::StdGen->Walls->Int->[Int]
-getRandoms ran (_:ws) rm=rn:(getRandoms ran2 ws rm)
-        where (rn,ran2)=randomR (-1*rm, 1*rm) ran
-getRandoms _ [] _=[]
-
-getWidths::StdGen->Walls->Int->[Int]
-getWidths ran (w:ws) y=width:(getWidths ran2 ws y)
-        where (width,ran2)
-                        |y<2=randomR (1,cols `div` 2) ran
-                        |otherwise=(length w-1, ran)
-getWidths _ [] _=[]
-
-getn::Int->[a]->a
-getn n xs = last (Prelude.take n xs)
-
-checkGameOver :: GameState -> Bool
-checkGameOver (GameState car wall point dir over ran lifes record) =   headX == 2 || headX == (cols-2) || 
-                      headY == (rows-1)||headY==3||
-                      (wasWallsHit (GameState car wall point dir over ran lifes record) && lifes<1)
-    where
-            (headX, headY) = head car
-
-data GameState = GameState      { getCar :: Car
-                                , getWalls :: Walls
-                                , getPoints :: Int
-                                , getDirection :: Direction
-                                , isGameOver :: Bool
-                                , getRandomStdGen :: StdGen
-                                , getLifes:: Int
-                                , getRecord::Int }
-
-changeDirection :: GameState -> Direction -> GameState
-changeDirection (GameState c w p _ g r l re) newDir = GameState c w p newDir g r l re
-
-initialGameState :: Bool->Int -> GameState
-initialGameState gameOver record= GameState   { getCar = [  (carX, carY), 
-                                                        (carX, carY - 1),
-                                                        (carX, carY - 2), 
-                                                        (carX - 1, carY - 2), 
-                                                        (carX + 1, carY - 2)]
-                                        , getWalls = [[(carX-2,rows-1),(carX-1,rows-1),(carX,rows-1),(carX+1,rows-1), (carX+2,rows-1)],[(cols,rows-1),(cols-1,rows-1),(cols-2,rows-1)],[(1,rows-1),(2,rows-1),(3,rows-1)]]
-                                        , getPoints =0
-                                        , getDirection = LEFT
-                                        , isGameOver = gameOver
-                                        , getRandomStdGen = mkStdGen 100 
-                                        , getLifes=9
-                                        , getRecord=record}
-        where   carX = cols `div` 2
-                carY = 4
+main :: IO ()
+main =do
+    a<-fmap (read::String->Int) (readFile "record.txt") 
+    playIO window background 10 (initialGameState False a) render handleKeys update
